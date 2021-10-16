966660c76fa79d941b0e43f049230e2369de3093
Nicola <nicspanu@gmail.com>
2021-10-16 08:00:27 +0200

    social site

--- django_env/social_site/core/urls.py
+++ django_env/social_site/core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage' )
+    path('', views.homepage, name='homepage' ),
+    path('users/', views.UserList.as_view(), name='user_list'),
+    path('user/<str:username>/', views.user_profile_view, name='user_profile'),
 ]
