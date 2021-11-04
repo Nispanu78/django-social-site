e691d4f42cbdc056324155d21fbcfb83803af769
Nicola <nicspanu@gmail.com>
2021-11-04 11:05:31 +0100

    mixins

--- django_env/social_site/forum/views.py
+++ django_env/social_site/forum/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .models import Sezione
+
+
+class CreaSezione(CreateView):
+    model = Sezione
+    fields = "__all__"
+    template_name = "forum/crea_sezione.html"
+    success_url = "/"
