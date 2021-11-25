673e863c678bf227d7ca7f7dc10439f4588b25c0
Nicola <nicspanu@gmail.com>
2021-11-25 12:19:52 +0100

    social site

--- django_env/social_site/core/views.py
+++ django_env/social_site/core/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
-from forum.models import Sezione
+from forum.models import Discussione, Sezione
 
 # def homepage(request):
 #     return render(request, 'core/homepage.html')
@@ -20,6 +20,7 @@ class UserList(LoginRequiredMixin, ListView):
 
 def user_profile_view(request, username):
     user = get_object_or_404(User, username=username)
-    context = {'user': user}
+    discussioni_utente = Discussione.objects.filter(autore_discussione=user).order_by("-pk")
+    context = {'user': user, 'discussioni_utente': discussioni_utente}
     return render(request, 'core/user_profile.html', context)
 
