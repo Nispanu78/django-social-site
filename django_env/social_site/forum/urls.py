843b47d42bb66a7a7845533f9f60e1813a77250f
Nicola <nicspanu@gmail.com>
2021-11-25 18:03:24 +0100

    social site

--- django_env/social_site/forum/urls.py
+++ django_env/social_site/forum/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('nuova-sezione/', views.CreaSezione.as_view(), name='crea-sezione'),
+    path('nuova-sezione/', views.CreaSezione.as_view(), name='crea_sezione'),
     path('sezione/<int:pk>/', views.visualizza_sezione, name='sezione_view'),
     path('sezione/<int:pk>/crea-discussione/', views.crea_discussione, name='crea_discussione'),
     path('discussione/<int:pk>/', views.visualizza_discussione, name='visualizza_discussione'),
