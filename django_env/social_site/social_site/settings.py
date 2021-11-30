355dd03d04eeafe51593f428538e69db031f742c
Nicola <nicspanu@gmail.com>
2021-11-30 15:59:55 +0100

    social site

--- django_env/social_site/social_site/settings.py
+++ django_env/social_site/social_site/settings.py
@@ -82,10 +82,19 @@ WSGI_APPLICATION = 'social_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social_site',
+        'USER': 'postgres',
+        'PASSWORD': '*******'
     }
 }
 
